efc8a2fbdc42a84fb42fe39bbf440742b1f6d3e2
Henrique Leite <henrique.leite@aarin.com.br>
2026-01-20 16:56:36 -0300

    commit

---
+++ infra/terraform/network.tf
@@ -0,0 +1,32 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "social_media"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_security_group" "social_media" {
+  name   = "social_media"
+  vpc_id = module.vpc.vpc_id
+
+  tags = {
+    Name = "social_media"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_postgres" {
+  security_group_id = aws_security_group.social_media.id
+  description       = "Allow Postgres from specific IP"
+  from_port         = 5432
+  to_port           = 5432
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "104.30.160.22/32"
+}
