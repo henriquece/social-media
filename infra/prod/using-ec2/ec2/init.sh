7ebe4dc2bac2fd56db34443c00f8544289ee101e
Henrique Leite <henrique.leite@aarin.com.br>
2026-02-01 12:37:15 -0300

    commit

--- infra/prod/using-ec2/ec2/init.sh
+++ infra/prod/using-ec2/ec2/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /social-media/infra/prod/using-ec2/docker-compose/
 
-sudo docker compose up server-http
+sudo docker compose up -d server-http
 
 sudo docker compose run --rm certbot -v certonly --webroot --webroot-path /var/www/certbot/ -d api.henriquece.dev
 
